8b6f22cb70093c935237c5ea6d2e901525b72d6c
Henrique Oliveira <henrique.olive29@gmail.com>
2021-11-15 15:58:17 +0000

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import styled from "styled-components";
+import Section from './Section';
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <Container>
+            <Section 
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            ></Section>
+            <Section 
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            ></Section>
+            <Section 
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            ></Section>
+             <Section 
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            ></Section>
+            
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.div`
+    height: 100vh;
+
+`
